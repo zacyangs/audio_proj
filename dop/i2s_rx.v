d7f94c4c4ca6b58ca0b117b2e2ba12ec4d133908
zack <zacyang@foxmail.com>
2022-10-19 09:59:23 +0800

    modify i2s

--- dop/i2s_rx.v
+++ dop/i2s_rx.v
@@ -7,50 +7,55 @@ module i2s_rx#(
     input               bclk,
     input               data,
 
-    output reg          valid = 0,
+    output reg          valid  = 0,
     output reg [DW-1:0] l_data = 0,
     output reg [DW-1:0] r_data = 0
 );
 
 reg [8:0] bcnt;// max 512 fs
 wire[8:0] bcnt_x;
+wire        valid_x;
 
 reg       lrck_q;
 wire      lrck_rise;
 wire      lrck_fall;
 
-reg [DW-1:0] l_shift = 0;
-reg [DW-1:0] r_shift = 0;
+reg [31:0] l_shift = 0;
+reg [31:0] r_shift = 0;
 
 
 assign bcnt_x = (rst | lrck_rise | lrck_fall)? 9'b0 : bcnt + 1'b1;
 
-always@(posedge bclk or posedge rst)
+always@(posedge bclk)
 begin
     if(lrck) //right channel
-        r_shift <= {r_shift[DW-2:0], data};
+        r_shift <= {r_shift[30:0], data};
     else
-        l_shift <= {l_shift[DW-2:0], data};
+        l_shift <= {l_shift[30:0], data};
 
     lrck_q <= lrck;
     
     bcnt <= bcnt_x;
 
-    if(lrck_rise && TYPE == "RJUST" ||
-       (bcnt == (DW) && TYPE == "I2S" ||
-       bcnt == (DW-1) && TYPE == "LJUST") && !lrck)
-        l_data <= l_shift;
+    if(lrck_fall)
+        r_data <= TYPE == "RJUST" ? r_shift[DW-1:0] :
+                  TYPE == "LJUST" ? r_shift[31 -: DW] : r_shift[30 -: DW];
 
-    if(lrck_fall && TYPE == "RJUST" ||
-       (bcnt == (DW) && TYPE == "I2S" ||
-       bcnt == (DW-1) && TYPE == "LJUST") && lrck) begin
-        r_data <= r_shift;
-        valid <= 1;
-    end
+    if(lrck_rise)
+        l_data <= TYPE == "RJUST" ? l_shift[DW-1:0] :
+                  TYPE == "LJUST" ? l_shift[31 -: DW] : l_shift[30 -: DW];
+end
+
+always@(posedge bclk or posedge rst)
+begin
+    if(rst)
+        valid <= 1'b0;
+    else if(lrck_rise)
+        valid <= 1'b1;
     else
         valid <= 1'b0;
-
 end
+
 
 assign lrck_rise = lrck & !lrck_q;
 assign lrck_fall = !lrck & lrck_q;
